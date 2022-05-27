0de67e4a052a22600166016d7f5992fe9354da95
redbor24 <redbor24@gmail.com>
2022-05-27 16:06:45 +0300

    add PokemonEntity model

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
-from pokemon_entities.models import Pokemon
+from pokemon_entities.models import Pokemon, PokemonEntity
 
 
 @admin.register(Pokemon)
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ('title',)
 
+
+@admin.register(PokemonEntity)
+class PokemonEntityAdmin(admin.ModelAdmin):
+    list_display = ('lat', 'lon')
 
--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.TextField(max_length=200, verbose_name='Pokemon')
     image = models.ImageField(upload_to='pokemons', null=True)
+
+
+class PokemonEntity(models.Model):
+    lat = models.FloatField()
+    lon = models.FloatField()
