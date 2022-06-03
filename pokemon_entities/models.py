a40913957c90a2a01d721c3080b0f6d6859ed763
redbor24 <redbor24@gmail.com>
2022-06-03 11:40:52 +0300

    Форматирование кода, удалено лишнее поле next_evolution, добавлен метод PokemonEntity.__str__()

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,32 +2,33 @@ from django.db import models
 
 
 class Pokemon(models.Model):
-    title_ru = models.CharField(max_length=200, verbose_name='Имя на русском', blank=True, default='')
-    title_en = models.CharField(max_length=200, verbose_name='Имя на английском', blank=True, default='')
-    title_jp = models.CharField(max_length=200, verbose_name='Имя на японском', blank=True, default='')
+    title_ru = models.CharField(max_length=200, verbose_name='Имя на русском', blank=True)
+    title_en = models.CharField(max_length=200, verbose_name='Имя на английском', blank=True)
+    title_jp = models.CharField(max_length=200, verbose_name='Имя на японском', blank=True)
     image = models.ImageField(upload_to='pokemons', null=True)
-    description = models.TextField(max_length=1024, verbose_name='Описание', blank=True, default='')
-    previous_evolution = models.ForeignKey('Pokemon', verbose_name='Предыдущий этап эволюции',
-                                           on_delete=models.DO_NOTHING, related_name='pokemon_prev_evol',
-                                           null=True, blank=True)
-    next_evolution = models.ForeignKey('Pokemon', verbose_name='Следующий этап эволюции',
-                                       on_delete=models.DO_NOTHING, related_name='pokemon_next_evol',
-                                       null=True, blank=True)
+    description = models.TextField(max_length=1024, verbose_name='Описание', blank=True)
+    previous_evolution = models.ForeignKey(
+        'Pokemon',
+        verbose_name='Предыдущий этап эволюции',
+        on_delete=models.DO_NOTHING,
+        related_name='prev_evol_pokemon',
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
-        return f'id: {self.id}, {self.title_ru}'
-
-    def img_url(self):
-        return self.image.url if self.image else None
-
-    def pokemon_id(self):
-        return self.id
+        return f'{self.id}, {self.title_ru}'
 
 
 class PokemonEntity(models.Model):
     lat = models.FloatField(verbose_name='Широта')
     lon = models.FloatField(verbose_name='Долгота')
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name='Покемон')
+    pokemon = models.ForeignKey(
+        Pokemon,
+        on_delete=models.CASCADE,
+        verbose_name='Покемон',
+        related_name='entity_pokemons'
+    )
     appeared_at = models.DateTimeField(verbose_name='Момент появления', blank=True)
     disappeared_at = models.DateTimeField(verbose_name='Момент исчезновения', blank=True)
     level = models.IntegerField(verbose_name='Уровень', default=0)
@@ -35,3 +36,6 @@ class PokemonEntity(models.Model):
     strength = models.IntegerField(verbose_name='Сила', default=0)
     defence = models.IntegerField(verbose_name='Защита', default=0)
     stamina = models.IntegerField(verbose_name='Выносливость', default=0)
+
+    def __str__(self):
+        return f'id: {self.id} ({self.pokemon})'
